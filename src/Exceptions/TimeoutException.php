986b8594c533da1bebf5eabe2d6bef9404dcc63e
Yannik <info.tebros@web.de>
2022-09-13 21:22:31 +0200

    Add MakesHttpRequests trait to send requests and handle API errors

---
+++ src/Exceptions/TimeoutException.php
@@ -0,0 +1,34 @@
+<?php
+
+class TimeoutException extends Exception
+{
+    /**
+     * The output returned from the operation.
+     *
+     * @var array
+     */
+    public $output;
+
+    /**
+     * Create a new exception instance.
+     *
+     * @param  array  $output
+     * @return void
+     */
+    public function __construct(array $output)
+    {
+        parent::__construct('Script timed out while waiting for the process to complete.');
+
+        $this->output = $output;
+    }
+
+    /**
+     * The output returned from the operation.
+     *
+     * @return array
+     */
+    public function output()
+    {
+        return $this->output;
+    }
+}
